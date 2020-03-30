5d71bfe224d1bfabfc8475eea886cc20d4ddd124
Faisal Shohag <50203997+faisalbrur@users.noreply.github.com>
2020-03-30 10:38:43 +0600

    Create update.js

---
+++ update.js
@@ -0,0 +1,66 @@
+const aff = document.querySelector('.pcount2');
+aff.innerHTML = "৭২২,১৯৬";
+
+const d = document.querySelector('.rcount2');
+d.innerHTML = "৩৩,৯৭৬";
+
+const r = document.querySelector('.gcount2');
+r.innerHTML = "১৫১,৭৬৬";
+
+const p = document.querySelector('.ocount2');
+p.innerHTML = "৫৩৬,৪৫৪";
+
+//const ld = 
+//document.querySelector('.ldcount');
+//ld.innerHTML = "৩,৫১৬";
+/*
+const rate = 
+document.querySelector('.rate');
+rate.innerHTML = "(প্রতি মিনিটে ২.৪৪ জন)";
+*/
+const marquee = document.querySelector('.marq');
+marquee.innerHTML = "★ ইতালিতে ২৪ঘন্টায় মৃত ৮৮৯; স্পেনে ৮৪৪ জন ★ নিউইয়র্ক কোয়ারেন্টিন জরুরি মনে করছেন না যুক্তরাষ্ট্রের প্রেসিডেন্ট ডোনাল্ড ট্রাম্প: বিবিসি ★";
+
+
+const country = document.querySelector('.count');
+country.innerHTML = "(১৯৯টি দেশে + ২টি সমুদ্র জাহাজ)";
+
+
+//last 24 hour update
+
+const cir1t = document.querySelector('.cir1t');
+cir1t.innerHTML = "৫৮,৮৬৪";
+
+const affm = document.querySelector('.affm');
+affm.innerHTML = "৪০.৮৭জন/মিনিট";
+
+const cir2t = document.querySelector('.cir2t');
+cir2t.innerHTML = "৩,১১০";
+
+const deadm = document.querySelector('.deadm');
+deadm.innerHTML = "২.১৫জন/মিনিট";
+
+
+
+const cir3t = document.querySelector('.cir3t');
+cir3t.innerHTML = "৩০%";
+
+const recm = document.querySelector('.recm');
+recm.innerHTML = "সর্বমোট হিসাব";
+
+
+const topaff = document.querySelector('.topaff');
+topaff.innerHTML = "যুক্তরাষ্ট্র  ১৮,৪৬৯ জন";
+
+
+const topdead = document.querySelector('.topdead');
+topdead.innerHTML = "স্পেন ৮২১ জন";
+
+const toptestbd = document.querySelector('.toptestbd');
+toptestbd.innerHTML = "১০৯ টি";
+
+const topaffbd = document.querySelector('.topaffbd');
+topaffbd.innerHTML = "০০ জন";
+
+const topdeadbd = document.querySelector('.topdeadbd');
+topdeadbd.innerHTML = "০০ জন";
